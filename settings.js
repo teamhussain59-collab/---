deece92629b39104515a665be176b1a1189fe921
◄⏤͟͟͞🍸⃝⃪🖤𝐇𝐔𝐒𝐒𝚲𝐈𝐍-𝐌𝐃 ᭓┣𔘓⃭𓆩🖤𓆪 <teamhussain59@gmail.com>
2026-01-20 20:38:18 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '◄⏤͟͟͞🍸⃝⃪🖤𝐇𝐔𝐒𝐒𝚲𝐈𝐍-𝐌𝐃 ᭓┣𔘓⃭𓆩🖤𓆪',
+  author: '‎',
+  botName: "◄⏤͟͟͞🍸⃝⃪🖤𝐇𝐔𝐒𝐒𝚲𝐈𝐍-𝐌𝐃 ᭓┣𔘓⃭𓆩🖤𓆪",
+  botOwner: 'SILVER', // Your name
+  ownerNumber: '923124015719', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/teamhussain59-collab/---",
+};
+
+module.exports = settings;
